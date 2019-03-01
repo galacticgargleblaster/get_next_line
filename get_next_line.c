06c4ce0def79876feba40b0063944e230b1666e2
Nicholas Kirkby <nkirkby@e1z4r15p3.42.us.org>
2019-03-01 11:41:19 -0800

    brb, debuffering mind

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: nkirkby <nkirkby@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/21 11:31:08 by nkirkby           #+#    #+#             */
-/*   Updated: 2019/03/01 10:59:50 by nkirkby          ###   ########.fr       */
+/*   Updated: 2019/03/01 11:40:13 by nkirkby          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,24 +35,33 @@ static gnl_context_t	*__gnl_context_new__(const int fd)
 	ft_bzero(context->buf, BUFF_SIZE);
 	context->line = NULL;
 	context->fd = fd;
+	context->line_size = 0;
 	context->read_return_value = BUFF_SIZE;
 	return (context);
 }
 
-static void				__gnl_context_del__(gnl_context_t *context)
+static int				debuffer_line(gnl_context_t *c)
 {
-	free(context->buf);
-	free(context);
-}
+	char				*tmp;
 
+	if ((tmp = ft_strnew(c->line_size)) == NULL)
+		return (GET_NEXT_LINE_READ_ERROR);
+	if (c->line)
+	{
+		if (ft_strcpy(tmp, c->line) == NULL)
+			return (GET_NEXT_LINE_READ_ERROR);
+		free(c->line);
+	}
+	if (ft_strncat(tmp, c->buf, c->line_size) == NULL)
+		return (GET_NEXT_LINE_READ_ERROR);
+	c->line = tmp;
+}
 
 #define CONTAINS_NEWLINE(STR) (ft_strchr(STR, '\n') != NULL)
 #define NOT(X) (!(X))
 
 static int				get_next_line_in_context(gnl_context_t *c, char **line)
 {
-	char			*tmp;
-
 	while ((c->read_return_value = read(c->fd, c->buf, BUFF_SIZE)) != 0)
 	{
 		if (c->read_return_value < 0)
@@ -60,15 +69,6 @@ static int				get_next_line_in_context(gnl_context_t *c, char **line)
 		c->line_size += (CONTAINS_NEWLINE(c->buf) ? 
 						(size_t)(ft_strchr(c->buf, '\n') - c->buf) : 
 						c->read_return_value);
-		if ((tmp = ft_strnew(c->line_size)) == NULL)
-			return (GET_NEXT_LINE_READ_ERROR);
-		if (c->line && 
-				(ft_strcpy(tmp, c->line) == NULL ||
-				ft_strncat(tmp, c->buf, c->line_size) == NULL))
-			return (GET_NEXT_LINE_READ_ERROR);
-		else
-			free(c->line);
-		c->line = tmp;
 		*line = c->line;
 		if (CONTAINS_NEWLINE(c->buf))
 			return (GET_NEXT_LINE_READ_SUCCESS);
@@ -100,7 +100,8 @@ int						get_next_line(const int fd, char **line)
 		return_value == GET_NEXT_LINE_READ_COMPLETE)
 	{
 		c = ft_lstremove(&contexts, c);
-		__gnl_context_del__(c);
+		free(c->buf);
+		free(c);
 	}
 	return (return_value);
 }
